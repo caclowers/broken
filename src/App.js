e615923cce95792e57ef01f5dad9fc0afedf42c1
Casey <caclowers@gmail.com>
2018-11-07 22:14:46 -0600

    added Sections component//added SectionA to App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
 import Header from './components/Header/Header.js';
-
+import SectionA from './components/Sections/SectionA.js';
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
+        <SectionA />
       </div>
     );
   }
---
+++ src/components/Sections/SectionA.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import '../../App.css';
+
+class SectionA extends Component {
+  render() {
+    return (
+      <div>
+        <p>bring your broken here.</p>
+        <p>broken things</p>
+        <p>broken ideals</p>
+        <p>broken spirit</p>
+      </div>
+    );
+  }
+}
+
+export default SectionA;
