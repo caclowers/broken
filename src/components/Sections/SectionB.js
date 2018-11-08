afb316908de0d5a255f1e7a6375267057ddf5e59
Casey <caclowers@gmail.com>
2018-11-07 22:24:50 -0600

    added favicon//added images folder//added broken glass to images folder

---
+++ src/components/Sections/SectionB.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../../App.css';
+
+class SectionB extends Component {
+  render() {
+    return (
+      <div>
+        <img src={} alt="broken-image-icon"></img>
+      </div>
+    );
+  }
+}
+
+export default SectionB;
