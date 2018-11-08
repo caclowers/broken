b1cce8a0758fcf38ee7c7d16fa5fa23cd5666bd8
Casey <caclowers@gmail.com>
2018-11-07 22:09:29 -0600

    added components folder//moved header into a component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../../App.css';
+
+class App extends Component {
+  render() {
+    return (
+        <header className="App-header">
+          <h1>Hello Dreamer</h1>
+        </header>
+    );
+  }
+}
+
+export default App;
