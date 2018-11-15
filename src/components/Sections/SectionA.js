212676b24dd11f9ba500c75f9822be887e77d1bb
Hhasa <caclowers@Caseys-MacBook.local>
2018-11-14 22:42:24 -0600

    small tweak

--- src/components/Sections/SectionA.js
+++ src/components/Sections/SectionA.js
@@ -5,10 +5,11 @@ class SectionA extends Component {
   render() {
     return (
       <div>
-        <p>bring your broken here.</p>
+        <p>lay your broken here.</p>
         <p>broken things</p>
         <p>broken ideals</p>
         <p>broken spirit</p>
+        <p>rest</p>
       </div>
     );
   }
